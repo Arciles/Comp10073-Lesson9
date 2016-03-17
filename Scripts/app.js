4e1fb66c7fb97bc82c97c9878b4f74dff4687b65
Esat IBIS <esat.taha.ibis@gmail.com>
2016-03-17 10:48:50 -0400

    added projects.html and projects.js and other bunch of stuff

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,47 +3,77 @@
 
     "use strict";
     var output = "";
-    var firstParagraph = document.getElementById("firstParagraph");
+    //var firstParagraph = document.getElementById("firstParagraph");
 
     // Instantiate new xhr object
     var request = new XMLHttpRequest();
-    request.open('GET', '../person.txt', true);
+    request.open('GET', '/Comp1073%20-%20Lesson9/people.txt', true);
     request.addEventListener('readystatechange', function() {
         if (request.readyState === 4) {
-            var Person = {}; /* var Person = new Object();   */
+			var people = {};
 
-            Person = JSON.parse(request.responseText);
-            
-            Person.sayHello = function() {
-                output += "<br><hr><br>" + Person.name + " says hello";
-            }
+			// Reading from people.txt file into people variable
+			people = JSON.parse(request.responseText);
+            var addressBook = people.addressbook;
 
-            // for every key in the Person object, loop...
-            for (var key in Person) {
+            var addressBookLenght = addressBook.length;
 
-                // check if the key is the familyNames array
-                if (key === "familyNames") {
-                    output += "<br>Family Names: <br>";
-                    output += "<hr><br>";
-                    output += "<ul>";
-                    for (var index = 0; index < Person.familyNames.length; index++) {
-                        output += "<li>" + Person.familyNames[index] + "</li>";
-                    } // for loop
-                    output += "</ul>";
-                } // if statement
-                else if (key === "sayHello") {
-                    Person.sayHello();
-                }
-                
-                // for all other cases do the following...
-                else {
+/*			for (var key in addressBook) {
+				console.log(addressBook[key]);
 
-                    output += Person[key] + "<br>";
-                } // else statement
 
-            } // for in
+				addressBook[key].sayHello = function() {
+					output += "<br><hr><br>" + addressBook[key].name + " says hello";
+				}
+			}*/
 
-            firstParagraph.innerHTML = output;
+			for (var person = 0; person < addressBookLenght; person++) {
+				var output = "";
+				addressBook[person].sayHello = function() {
+					output += "<br><hr><br>" + this.name + " says hello";
+				};
+
+				/*
+				var Person = {}; /!* var Person = new Object();   *!/
+
+				Person = JSON.parse(request.responseText);
+
+				Person.sayHello = function() {
+					output += "<br><hr><br>" + Person.name + " says hello";
+				}*/
+
+				// for every key in the Person object, loop...
+				for (var key in addressBook[person]) {
+
+					// check if the key is the familyNames array
+					if (key === "familyNames") {
+						output += "<br>Family Names: <br>";
+						output += "<hr><br>";
+						output += "<ul>";
+
+						for (var index = 0; index < addressBook[person].familyNames.length; index++) {
+							output += "<li>" + addressBook[person].familyNames[index] + "</li>";
+						} // for loop
+
+						output += "</ul>";
+					} // if statement
+					else if (key === "sayHello") {
+						addressBook[person].sayHello();
+					}
+
+					// for all other cases do the following...
+					else {
+
+						output += addressBook[person][key] + "<br>";
+					} // else statement
+
+				} // for in
+				var paragraph = document.getElementById("paragraph" + person);
+				paragraph.innerHTML = output;
+
+				console.log(addressBook[index]);
+			}// outer for loop
+
         }
     });
 
---
+++ Scripts/projects.js
@@ -0,0 +1,12 @@
+/**
+ * Created by esattahaibis on 2016-03-17.
+ */
+
+
+(function () {
+
+	"use strict";
+
+	console.log("deneme");
+
+})();
