32fbece8b4af515c014c9447cd0378244742aba4
Esat IBIS <esat.taha.ibis@gmail.com>
2016-03-17 13:28:20 -0400

    projects.js has an error

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -1,12 +1,35 @@
-/**
- * Created by esattahaibis on 2016-03-17.
- */
-
-
 (function () {
+	/*console.log("step1");
 
-	"use strict";
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', '/Comp1073%20-%20Lesson9/projects.json', true);
+	console.log("step2");
+	xhr.addEventListener('readystatechange', function () {
+		console.log("step3");
+		if (xhr.readyState === 4) {
+			var projects = {};
+			var paragraph = [];
 
-	console.log("deneme");
+			console.log(xhr.responseText);
 
+			/!*projects = JSON.parse(request.responseText);
+			 console.log(projects);
+
+			 paragraph = projects.paragraphs;
+
+			 var paragraphLenght = paragraph.length;
+
+			 console.log(paragraph);
+			 for(var index = 0; index < paragraphLenght; index++) {
+			 document.getElementById("paragraph" + (index + 1)).innerHTML = paragraph[index];
+			 }*!/
+		}
+	});
+	console.log("step4");*/
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', '/Comp1073%20-%20Lesson9/projects.json', true);
+	xhr.onreadystatechange = function() {
+		console.log('ready');
+	}
 })();
